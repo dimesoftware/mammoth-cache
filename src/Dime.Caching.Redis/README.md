12cc9eb5a80b4227f7f1366e4bacfbed8d4e1cbf
Hendrik Bulens <hendrik.bulens@gmail.com>
2023-03-03 22:40:37 +0100

    Add Redis (#6)

---
+++ src/Dime.Caching.Redis/README.md
@@ -0,0 +1,21 @@
+# Redis Caching Decorator
+
+Simple caching contracts and implementation using existing caching frameworks.
+
+## About this project
+
+This project provides a framework agnostic `ICache` interface which contains a set of methods such as `Set<T>` and `Get<T>`. 
+This package is an implementation of this contract for **Redis** using the popular `StackExchange.Redis` package.
+	
+## Installation
+
+`dotnet add package Dime.Caching.Redis`
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+Please make sure to update tests as appropriate.
+
+# License
+
+[![License](http://img.shields.io/:license-mit-brightgreen.svg?style=flat-square)](http://badges.mit-license.org)
---
+++ src/Dime.Caching.Redis/RedisCacheDecorator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using StackExchange.Redis;
+
+namespace Dime.Caching.Redis
+{
+    public class RedisCacheDecorator : ICache
+    {
+        protected readonly IDatabase _cache;
+
+        public RedisCacheDecorator(IConnectionMultiplexer redis)
+        {
+            _cache = redis.GetDatabase();
+        }
+
+        protected virtual string GetKey(string key) => key;
+
+        public virtual T Get<T>(string key)
+        {
+            RedisValue value = _cache.StringGet(GetKey(key));
+            return !value.IsNull ? JsonSerializer.Deserialize<T>(value) : default;
+        }
+
+        public virtual void Remove(string key)
+            => _cache.KeyDelete(GetKey(key));
+
+        public virtual void Set<T>(string key, T value, TimeSpan? expiry)
+            => _cache.StringSetAsync(
+                GetKey(key),
+                JsonSerializer.Serialize(value, new JsonSerializerOptions { ReferenceHandler = ReferenceHandler.IgnoreCycles }),
+                expiry ?? TimeSpan.FromHours(1));
+    }
+}
