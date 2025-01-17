91db2a7452f56f12de8cdaa939c2a26472df5b8e
hbulens <hendrik.bulens@gmail.com>
2025-01-17 17:49:18 +0100

    Pub/sub cache invalidation

---
+++ src/MammothCache.Redis/RedisCacheDecorator.Subscribe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace MammothCache
+{
+    public partial class RedisCacheDecorator
+    {
+        public void Subscribe(string channel, Action<string> onInvalidated)
+        {
+            ISubscriber subscriber = _connectionMultiplexer.GetSubscriber();
+            RedisChannel channelWithPattern = new(channel, RedisChannel.PatternMode.Literal);
+            subscriber.Subscribe(channelWithPattern, (channel, key) => onInvalidated(key));
+        }
+
+        public Task PublishAsync(string channel, string key)
+        {
+            ISubscriber subscriber = _connectionMultiplexer.GetSubscriber();
+            RedisChannel channelWithPattern = new(channel, RedisChannel.PatternMode.Literal);
+            return subscriber.PublishAsync(channelWithPattern, key);
+        }
+    }
+}
